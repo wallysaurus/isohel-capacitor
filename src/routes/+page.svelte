744bfe9d6f746e2e960d1e3a4978100c3fb4780c
wally <69739715+wallysaurus@users.noreply.github.com>
2024-04-30 00:19:40 -0500

    some circle stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,56 @@
-<script>
-    import { Geolocation } from '@capacitor/geolocation';
+<script lang="ts">
+    import Circle from './components/circle.svelte';
+    import { onMount } from 'svelte';
+    import { gsap } from 'gsap';
 
-    /**
-	 * @type {import("@capacitor/geolocation").Position | null}
-	 */
-    let loc = null;
-    async function getCurrentPosition(){
-        const res = await Geolocation.getCurrentPosition()
-        loc = res
+    let circles : any[] = [];
+    let innerWidth : number;
+    let innerHeight : number;
+    let circle : any;
+
+    onMount(() => {
+        innerWidth = window.innerWidth;
+        innerHeight = window.innerHeight;
+        if (circle) {
+            circle.move();
+        }
+    });
+
+    function getRandPos(circleSize : number) {
+        const randX = Math.random() * (window.innerWidth - circleSize);
+        const randY = Math.random() * (window.innerHeight - circleSize);
+
+        return { x: randX, y: randY }
+    }
+
+    function onClick() { 
+        const randPos = getRandPos(25)
+        const newCircle = { 
+            x: randPos.x,
+            y: randPos.y,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+        }
+        circles.push(newCircle);
+        circles = circles
     }
 </script>
 
-<div>
-    <h1>Geolocation</h1>
-    <p>Your location is:</p>
-    <p>Latitude: {loc?.coords.latitude}</p>
-    <p>Longitude: {loc?.coords.longitude}</p>
-
-    <button on:click={getCurrentPosition}>
-        Get Current Location
-    </button>
-</div>
+<svg viewBox={`0 0 ${innerWidth} ${innerHeight}`} xmlns="http://www.w3.org/2000/svg">
+    {#each circles as circle}
+        <Circle on:componentLoaded={circle} x={circle.x} y={circle.y} color={circle.color} radius={200 - (Math.random() * 100)} />
+    {/each}
+</svg>
+<div id="blur-overlay" />
+<button on:click={onClick} style="position:fixed; top:50vh; left: calc(50vw - 50px);" >Create Circle</button>
 
 <style>
-    div {
-        padding-top: 50px;
-    }
+    #blur-overlay {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+
+        backdrop-filter: blur(50px);
+	}
 </style>
---
+++ src/routes/components/circle.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { gsap } from 'gsap';
+
+	export let x = 0;
+	export let y = 0;
+	export let radius = 20;
+	export let color = 'red';
+
+    export function move() {
+        console.log("poo")
+        gsap.to(".circle", {color: "#8c0", duration: 1});
+    }
+</script>
+
+<style>
+    .circle {
+        fill: var(--color);
+        stroke: none;
+        width: calc( var(--radius) * 1px );
+        height: calc( var(--radius) * 1px );
+        border-radius: 50%;
+        position: absolute;
+        top: calc( var(--y) * 1px );
+        left: calc( var(--x) * 1px );
+    }
+</style>
+  
+<circle class="circle" cx="{x}" cy="{y}" r="{radius}" style:--color={color} style:--x={x} style:--y={y} style:--radius={radius} />
