c3113c15782f858ffe7e3c1f8e681e8723096b79
wally <69739715+wallysaurus@users.noreply.github.com>
2024-05-01 17:02:23 -0500

    blurs n movement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,40 @@
 <script lang="ts">
     import Circle from './components/circle.svelte';
     import { onMount } from 'svelte';
-    import { gsap } from 'gsap';
+    import Color from 'colorjs.io';
 
     let circles : any[] = [];
     let innerWidth : number;
     let innerHeight : number;
-    let circle : any;
 
     onMount(() => {
         innerWidth = window.innerWidth;
         innerHeight = window.innerHeight;
-        if (circle) {
-            circle.move();
+        for (let i = 0; i < 20; i++) {
+            instantiateCircle();
         }
     });
 
     function getRandPos(circleSize : number) {
         const randX = Math.random() * (window.innerWidth - circleSize);
         const randY = Math.random() * (window.innerHeight - circleSize);
-
         return { x: randX, y: randY }
     }
 
-    function onClick() { 
-        const randPos = getRandPos(25)
-        const newCircle = { 
+    function getRandColor() {
+        let colors = ['#fc0303', '#fc6b03', '#fcd303', '#a1fc03', '#03fc3d', '#03fcce', '#03adfc', '#0320fc', '#6203fc', '#8c03fc', '#e303fc', '#fc03b1'];
+        let color = new Color(colors[Math.floor(Math.random()*colors.length)]).to("hsl");
+        color.s = color.s * 0.8;
+        return color.toString();
+    }
+
+    function instantiateCircle() {
+        const randPos = getRandPos(25);
+        const newCircle = {
             x: randPos.x,
             y: randPos.y,
-            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+            r: 200 - (Math.random() * 50),
+            color: getRandColor()
         }
         circles.push(newCircle);
         circles = circles
@@ -36,21 +42,22 @@
 </script>
 
 <svg viewBox={`0 0 ${innerWidth} ${innerHeight}`} xmlns="http://www.w3.org/2000/svg">
-    {#each circles as circle}
-        <Circle on:componentLoaded={circle} x={circle.x} y={circle.y} color={circle.color} radius={200 - (Math.random() * 100)} />
+    {#each circles as attr}
+        <Circle {attr} innerWidth={innerWidth} innerHeight={innerHeight} />
     {/each}
 </svg>
-<div id="blur-overlay" />
-<button on:click={onClick} style="position:fixed; top:50vh; left: calc(50vw - 50px);" >Create Circle</button>
+
+<!-- Blur Filter -->
+<div />
+<button on:click={instantiateCircle} style="position:fixed; top:50vh; left: calc(50vw - 50px);" >Create Circle</button>
 
 <style>
-    #blur-overlay {
+    div {
         position: fixed;
-        left: 0;
         top: 0;
+        left: 0;
         width: 100%;
         height: 100%;
-
-        backdrop-filter: blur(50px);
-	}
+        backdrop-filter: blur(40px);
+    }
 </style>
--- src/routes/components/circle.svelte
+++ src/routes/components/circle.svelte
@@ -1,28 +1,59 @@
-<script>
+<script lang="ts">
     import { gsap } from 'gsap';
+    import { onMount } from 'svelte';
+    import Color from 'colorjs.io';
 
-	export let x = 0;
-	export let y = 0;
-	export let radius = 20;
-	export let color = 'red';
+    export let innerWidth : number;
+    export let innerHeight : number;
+    export let attr : any;
+    let circle : any;
 
-    export function move() {
-        console.log("poo")
-        gsap.to(".circle", {color: "#8c0", duration: 1});
+    onMount(() => { if (circle) { move(); } });
+
+    function getRandColor() {
+        let colors = ['#fc0303', '#fc6b03', '#fcd303', '#a1fc03', '#03fc3d', '#03fcce', '#03adfc', '#0320fc', '#6203fc', '#8c03fc', '#e303fc', '#fc03b1'];
+        let color = new Color(colors[Math.floor(Math.random()*colors.length)]).to("hsl");
+        color.s = color.s * 0.5;
+        return color.toString();
+    }
+    
+    function move() {
+        const newPos = {
+            x: (Math.random() * (innerWidth + 50)) - 25,
+            y: (Math.random() * (innerHeight + 50)) - 25
+        }
+        const color = getRandColor();
+        gsap.to(circle, {
+            cx: newPos.x,
+            duration: "random(5, 10)",
+            onComplete: () => {
+                gsap.set(circle, {cx: newPos.x})
+            },
+            ease: "power1.inOut"
+        });
+        gsap.to(circle, {
+            cy: newPos.y,
+            duration: "random(5, 10)",
+            onComplete: () => {
+                gsap.set(circle, {cy: newPos.y})
+            },
+            ease: "power4.inOut"
+        });
+        gsap.to(circle, {
+            fill: color,
+            duration: "random(5, 10)",
+            onComplete: () => {
+                gsap.set(circle, {fill: color})
+                move()
+            },
+            ease: "power1.inOut"
+        });
     }
 </script>
 
-<style>
-    .circle {
-        fill: var(--color);
-        stroke: none;
-        width: calc( var(--radius) * 1px );
-        height: calc( var(--radius) * 1px );
-        border-radius: 50%;
-        position: absolute;
-        top: calc( var(--y) * 1px );
-        left: calc( var(--x) * 1px );
-    }
-</style>
-  
-<circle class="circle" cx="{x}" cy="{y}" r="{radius}" style:--color={color} style:--x={x} style:--y={y} style:--radius={radius} />
+<circle bind:this={circle} cx={attr.x} cy={attr.y} r={attr.r}
+    style={`
+        fill: ${attr.color};
+        stroke: 'none';
+    `}
+/>
