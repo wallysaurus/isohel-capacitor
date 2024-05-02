d2075bf1d6226a4704277d9e80a9bf397f598310
wallysaurus <wallyminehut@gmail.com>
2024-05-01 23:48:30 -0500

    cleanup

--- android/app/src/main/java/me/wally/isohel/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package me.wally.isohel;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- dist/_app/immutable/entry/start.CA6TEnlt.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DQwzAvEG.js";export{t as start};
